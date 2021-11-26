0485aa7e7438005ec10b8f4019b7b003de1990a8
Tabary Mathieu <43238044+TabaryM@users.noreply.github.com>
2021-11-26 17:54:09 +0100

    Prepared for a good API implementation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,50 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {Logger, ValidationPipe} from "@nestjs/common";
+import {
+  FastifyAdapter,
+  NestFastifyApplication
+} from "@nestjs/platform-fastify";
+import * as Config from 'config';
+import {AppConfig, SwaggerConfig} from './app.types';
+import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+async function bootstrap(config: AppConfig, swaggerConfig: SwaggerConfig) {
+  // create NestJS application
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter({logger: true}),
+    );
+  
+  // use global pipe validation
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+  
+  // create swagger options
+  const options = new DocumentBuilder()
+    .setTitle(swaggerConfig.title)
+    .setDescription(swaggerConfig.description)
+    .setVersion(swaggerConfig.version)
+    .addTag(swaggerConfig.tag)
+    .build();
+    
+  const apiDocument = SwaggerModule.createDocument(app, options, {
+    include: [AppModule/*TODO add the Module linked to the API*/],
+  })
+  
+  SwaggerModule.setup(swaggerConfig.path, app, apiDocument);
+  
+  await app.listen(config.port, config.host);
+  Logger.log(
+    `Application served at http://${config.host}:${config.port}`,
+    'boostrap');
 }
-bootstrap();
+
+bootstrap(
+  Config.get<AppConfig>('server'),
+  Config.get<SwaggerConfig>('swagger'),
+);
