9e5bf33e2e96046ca94aa8d2bb7b8edf520940ea
Tabary Mathieu <43238044+TabaryM@users.noreply.github.com>
2021-11-28 11:47:54 +0100

    Ajout de l'operation getById

--- src/recipe/recipe.service.ts
+++ src/recipe/recipe.service.ts
@@ -1,25 +1,55 @@
-import { Injectable } from '@nestjs/common';
+import {Injectable, NotFoundException} from '@nestjs/common';
 import {Recipe} from "./recipe.type";
 import {RECIPES} from '../static/_recipes'
-import {Observable, of} from "rxjs";
+import {find, from, mergeMap, Observable, of, throwError} from "rxjs";
 import {map} from "rxjs/operators";
+import {RecipeEntity} from "./entities/recipe.entity";
 
 @Injectable()
 export class RecipeService {
   private _recipes: Recipe[];
-  
+
+  /**
+   * Constructor of the class
+   */
   constructor() {
-    this._recipes =[].concat(RECIPES);
+    this._recipes = [].concat(RECIPES);
   }
-  
-  findAll = (): Observable<Recipe[] | void> =>
+
+  /**
+   * Return all the recipes in database
+   *
+   * @returns {Observable<RecipeEntity[] | void>} array with all the recipes or void if there is none
+   */
+  findAll = (): Observable<RecipeEntity[] | void> =>
     of(this._recipes).pipe(
       map((_: Recipe[]) => (!!_ && !!_.length ? _ : undefined)),
     );
-  
+
   /**
    * Return the first recipe known
+   *
+   * @returns {Observable<RecipeEntity | void>} first recipe in the database
    */
-  findFirst = (): Observable<Recipe | void> =>
+  findFirst = (): Observable<RecipeEntity | void> =>
     of(this._recipes[0]);
+
+  /**
+   * Returns the recipe with the corresponding id
+   *
+   * @param {string} id of the recipe
+   *
+   * @returns {Observable<RecipeEntity>}
+   */
+  findById = (id: string): Observable<RecipeEntity> =>
+    from(this._recipes).pipe(
+      find((_: Recipe) => _.id === id),
+      mergeMap((_: Recipe) =>
+        !!_
+        ? of(new RecipeEntity(_))
+        : throwError(
+            () => new NotFoundException(`Recipe with id '${id} not found`),
+          ),
+      ),
+    );
 }
