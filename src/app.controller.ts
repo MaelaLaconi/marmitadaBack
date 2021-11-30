b51d0ad15f564611526badcd9e06b12a43b88af5
Tabary Mathieu <43238044+TabaryM@users.noreply.github.com>
2021-11-30 11:04:35 +0100

    Authentification~ish

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -7,13 +7,13 @@ import { LocalAuthGuard } from "./auth/local-auth.guard";
 export class AppController {
   constructor(private readonly authService: AuthService) {}
 
-  //@UseGuards(LocalAuthGuard)
+  @UseGuards(LocalAuthGuard)
   @Post('auth/login')
-  async login(@Body() req) {
+  async login(@Request() req) {
     return this.authService.login(req.user);
   }
 
-  //@UseGuards(JwtAuthGuard)
+  @UseGuards(JwtAuthGuard)
   @Get('profile')
   getProfile(@Body() req) {
     return req.user;
